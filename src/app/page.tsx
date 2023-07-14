9fb234c01b4f6826601f552c5729426a08c8334d
TomMahe <mahettom@gmail.com>
2023-07-14 19:02:26 +0200

    redirect to servercomponent using push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
-import { useState } from "react"
-
+import { FormEvent, useState } from "react"
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
 
   const [inputValue, setInputValue] = useState('')
+  // —————————————————————————————————————————————————————— Push provide a way to redirect using Next routing
+  const { push } = useRouter()
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    // —————————————————————————————————————————————————————— Redirect our page into the dynamic route needed
+    push(`/prediction/${inputValue}`)
+  }
 
   return (
 
@@ -14,10 +23,21 @@ export default function Home() {
       <div>
         <h1>Enter your name</h1>
       </div>
-      <form>
-        <input type='text' placeholder='Type your name' />
-        <button type='submit' >Predict data</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          className='text-black'
+          placeholder='Type your name'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+
+        <button type='submit'>
+          Predict data
+        </button>
       </form>
+
     </div>
 
   )
