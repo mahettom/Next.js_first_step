36d7bf4642564e9ad083384e2f8c3dcb4db2859a
TomMahe <mahettom@gmail.com>
2023-07-14 18:22:26 +0200

    delete file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+'use client';
 
+import { useState } from "react"
 
 
 
 export default function Home() {
+
+  const [inputValue, setInputValue] = useState('')
+
   return (
 
     <div className='bg-black flex flex-col justify-center items-center h-screen w-full'>
-      Home
+      <div>
+        <h1>Enter your name</h1>
+      </div>
+      <form>
+        <input type='text' placeholder='Type your name' />
+        <button type='submit' >Predict data</button>
+      </form>
     </div>
 
   )
