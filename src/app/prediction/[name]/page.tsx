58654dc4beab0bc4979f4fdd21ba8ae1fa644c75
TomMahe <mahettom@gmail.com>
2023-07-14 19:18:25 +0200

    reception promise and resolve them

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -21,10 +21,14 @@ interface Params {
     params: { name: string }
 }
 
-export default function Page({ params }: Params) {
+export default async function Page({ params }: Params) {
+    // ————————————————————————————————————————————————————————— Reception promise ———————————————————————————
+    const ageData = getPredictedAge(params.name)
+    const genderData = getPredictedGender(params.name)
+    const countryData = getPredictedCountry(params.name)
 
-
-
+    // ————————————————————————————————————————————————————————— Resolve all promises ————————————————————————
+    const [age, gender, country] = await Promise.all([ageData, genderData, countryData])
 
     return (
 
